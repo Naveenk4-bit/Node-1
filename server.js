2d176a4ce76248447bc68e005144764b836c0224
Naveenk4-bit <antonynaveen73@gmail.com>
2024-07-15 21:33:23 +0530

    n2

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 const http=require("http");
 
 const port = 8080;
+
+const todoList = ["naveen","prem","raja","dinesh","melwin"];
+
 http.createServer((req,res) => {
-    res.writeHead(200,{"Content-Type": "text/html"});
-    res.write("<h1>Finaly you run this project naveen congrats </h1>");
+   const {method,url} = req;
+   if(url === "/todos") {
+    if(method === "GET"){
+        res.writeHead(200);
+        res.write(todoList.toString())
+    }
+    else if(method === "POST") {
+          let body = " ";
+          req.on('error',(err) => {
+            console.error(err)
+          }).on('data',(chunk) => {
+                 body += chunk;
+                 console.log("peace of data", chunk);
+          }).on('end', () => {
+            body = JSON.parse(body)
+            console.log("body data", body);
+            let newTodo = todoList;
+            newTodo.push(body.item);
+          
+          })
+    }
+    else{
+        res.writeHead(501);
+    }
+   
+}
+else if(url ==="/"){
+
+}
+
     res.end();
 })
 .listen(port,() => {
